a135aca68a4c529d9bd738e839a5cdf0505f6b79
Markus Villalobos <MarkusVC64@gmail.com>
2020-06-21 18:39:55 -0600

    Created Game Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public static GameManager Instance;
+
+    void Awake()
+    {
+        InitializeGameManager();
+    }
+
+    private void InitializeGameManager()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
