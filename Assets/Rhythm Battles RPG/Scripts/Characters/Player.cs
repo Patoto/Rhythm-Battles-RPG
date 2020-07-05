08572d4a7426c99570d6401b91834a56bc2d18e0
Markus Villalobos <MarkusVC64@gmail.com>
2020-07-05 17:51:25 -0600

    Working on player

---
+++ Assets/Rhythm Battles RPG/Scripts/Characters/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody Rigidbody;
+
+    private void Update()
+    {
+        ApplyGravity();
+    }
+
+    public void Jump(float jumpForce)
+    {
+        float newYVelocity = Rigidbody.velocity.y + jumpForce;
+        Rigidbody.velocity = Utils.ModifyVector3YValue(Rigidbody.velocity, newYVelocity);
+    }
+
+    private void ApplyGravity()
+    {
+        float relativeGravity = Utils.Gravity * Time.deltaTime;
+        float newYVelocity = Rigidbody.velocity.y - relativeGravity;
+        Rigidbody.velocity = Utils.ModifyVector3YValue(Rigidbody.velocity, newYVelocity);
+    }
+}
--- Assets/Rhythm Battles RPG/Scripts/Others/Utils.cs
+++ Assets/Rhythm Battles RPG/Scripts/Others/Utils.cs
@@ -2,10 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CoordinateTypes
+{
+    x,
+    y,
+    z
+}
+
 public class Utils : MonoBehaviour
 {
+    public static readonly float Gravity = 1;
+
     public static float BPMToSeconds(float bpm)
     {
         return 60 / bpm;
+    }
+
+    private static Vector3 ModifyVector3Value(Vector3 vector, CoordinateTypes coordinate, float newValue)
+    {
+        Vector3 modifiedVector3 = vector;
+        switch (coordinate)
+        {
+            case CoordinateTypes.x:
+                modifiedVector3 = new Vector3(newValue, vector.y, vector.z);
+                break;
+            case CoordinateTypes.y:
+                modifiedVector3 = new Vector3(vector.x, newValue, vector.z);
+                break;
+            case CoordinateTypes.z:
+                modifiedVector3 = new Vector3(vector.x, vector.y, newValue);
+                break;
+        }
+        return modifiedVector3;
+    }
+
+    public static Vector3 ModifyVector3XValue(Vector3 vector, float newValue)
+    {
+        return ModifyVector3Value(vector, CoordinateTypes.x, newValue);
+    }
+
+    public static Vector3 ModifyVector3YValue(Vector3 vector, float newValue)
+    {
+        return ModifyVector3Value(vector, CoordinateTypes.y, newValue);
+    }
+
+    public static Vector3 ModifyVector3ZValue(Vector3 vector, float newValue)
+    {
+        return ModifyVector3Value(vector, CoordinateTypes.z, newValue);
     }
 }
