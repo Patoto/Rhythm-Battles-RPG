2a1d797a4f63c0e2e5701de692facc8fca89e310
Markus Villalobos <MarkusVC64@gmail.com>
2020-07-09 00:13:10 -0600

    Implemented koreographer

--- Assets/Rhythm Battles RPG/Scripts/Characters/Player.cs
+++ Assets/Rhythm Battles RPG/Scripts/Characters/Player.cs
@@ -4,25 +4,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("References")]
-    public Animator Animator;
-
-    public enum HopDirection
+    private enum HopDirection
     {
         Left,
         Right
     }
 
-    public void Hop(HopDirection hopDirection)
+    private enum HitDirection
     {
+        Left,
+        Right
+    }
+
+    [Header("References")]
+    public Animator Animator;
+
+    private HopDirection lastHopDirection = HopDirection.Right;
+    private HitDirection lastHitDirection = HitDirection.Right;
+
+    public void Hop()
+    {
+        HopDirection hopDirection = GetNewHopDirection();
         switch (hopDirection)
         {
             case HopDirection.Left:
-                Animator.Play("HopLeft");
+                Animator.SetTrigger("HopLeft");
                 break;
             case HopDirection.Right:
-                Animator.Play("HopRight");
+                Animator.SetTrigger("HopRight");
                 break;
         }
+        lastHopDirection = hopDirection;
+    }
+
+    private HopDirection GetNewHopDirection()
+    {
+        HopDirection newHopDirection = HopDirection.Left;
+        if (lastHopDirection == HopDirection.Left)
+        {
+            newHopDirection = HopDirection.Right;
+        }
+        return newHopDirection;
+    }
+
+    public void Hit()
+    {
+        HitDirection hitDirection = GetNewHitDirection();
+        switch (hitDirection)
+        {
+            case HitDirection.Left:
+                Animator.SetTrigger("HitLeft");
+                break;
+            case HitDirection.Right:
+                Animator.SetTrigger("HitRight");
+                break;
+        }
+        lastHitDirection = hitDirection;
+    }
+
+    private HitDirection GetNewHitDirection()
+    {
+        HitDirection newHitDirection = HitDirection.Left;
+        if (lastHitDirection == HitDirection.Left)
+        {
+            newHitDirection = HitDirection.Right;
+        }
+        return newHitDirection;
     }
 }
--- Assets/Rhythm Battles RPG/Scripts/Managers/SceneManagers/TestSceneManager.cs
+++ Assets/Rhythm Battles RPG/Scripts/Managers/SceneManagers/TestSceneManager.cs
@@ -5,5 +5,30 @@ using UnityEngine;
 
 public class TestSceneManager : MonoBehaviour
 {
-    public Koreographer Koreographer;
+	[Header("References")]
+	public Player Player;
+	[Header("Others")]
+	[EventID] public string BeatEventID;
+	[EventID] public string HitEventID;
+
+	private void Start()
+	{
+		Initialize();
+	}
+
+	private void Initialize()
+	{
+		Koreographer.Instance.RegisterForEvents(BeatEventID, MakePlayerHop);
+		Koreographer.Instance.RegisterForEvents(HitEventID, MakePlayerHit);
+	}
+
+	private void MakePlayerHop(KoreographyEvent koreographyEvent)
+	{
+		Player.Hop();
+	}
+
+	private void MakePlayerHit(KoreographyEvent koreographyEvent)
+	{
+		Player.Hit();
+	}
 }
